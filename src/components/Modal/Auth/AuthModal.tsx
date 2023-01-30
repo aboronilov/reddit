54fbb5774210d5fb60f6cb2dc1dbe47f1b9c8e63
Anatoly <boronilov@gmail.com>
2023-01-30 12:18:10 +0200

    auth modal UI

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,26 +1,59 @@
 import { authModalState } from "@/atoms/authModalAtom";
-import { Button, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+} from "@chakra-ui/react";
 import React from "react";
 import { useRecoilState } from "recoil";
+import AuthInputs from "./AuthInputs";
+import OAuthButtons from "./OAuthButtons";
 
 const AuthModal = () => {
-  const [modalState, setModalState] = useRecoilState(authModalState)
+  const [modalState, setModalState] = useRecoilState(authModalState);
 
   const handleClose = () => {
-    setModalState(prev => ({
-        ...prev,
-        open: false
-    }))
-  }
+    setModalState((prev) => ({
+      ...prev,
+      open: false,
+    }));
+  };
   return (
     <>
       <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader textAlign="center">
+            {modalState.view === "login" && "Login"}
+            {modalState.view === "signup" && "Signup"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            Hello I am a modal body
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+            pb={5}
+          >
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              width="70%"
+            >
+                <OAuthButtons />
+                <Text color="gray.500" fontWeight={700}>OR</Text>
+                <AuthInputs />
+                {/* <ResetPassword /> */}
+            </Flex>
           </ModalBody>
         </ModalContent>
       </Modal>
